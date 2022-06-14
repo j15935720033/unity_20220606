35c9bf66eed711095be9ebdc3225a35839375c98
j15935720033 <99215381@ulive.pccu.edu.tw>
2022-06-14 16:27:25 +0800

    update
    不重要

--- unity_20220606/Assets/Script/APIstatic.cs
+++ unity_20220606/Assets/Script/APIstatic.cs
@@ -7,6 +7,7 @@ namespace chia
         private void Start()
         {
             #region 練習
+            /*
             //取得靜態屬性
             //語法:
             //類別.靜態屬性名稱
@@ -27,19 +28,42 @@ namespace chia
             int rangeInt = Random.Range(1, 3);//不包含3
             print("隨機整數1~2" + rangeInt);
             #endregion
+            */
+            #endregion
 
             #region C# 靜態屬性與方法練習 2022.06.13
             //取得所有攝影機數量
             int AllCameraNum = Camera.allCameras.Length;
             //取得應用程式的平台
             string application = Application.platform.ToString();
+            //取得2D物理睡眠臨界值設定為 10
+            Physics2D.timeToSleep=10;
+            //時間大小設定為 0.5(慢動作)
+            Time.fixedDeltaTime = 0.5f;
 
 
+
+
+
+            print("所有攝影機數量"+ AllCameraNum);
+            print("應用程式的平台" + application);
+            print("取得2D物理睡眠臨界值" + Physics2D.timeToSleep);
             #endregion
         }
         private void Update()
         {
-            
+            //取得是否輸入任意鍵
+            //bool getAnyKey =Input.anyKey;
+            if (Input.anyKey)
+            {
+                print("A key or mouse click has been detected");
+            }
+            //取得遊戲經過時間
+            float GameTime=Time.time;
+            //print("<Color=yellow >遊戲時間:</Color >"+ GameTime);
+           
+           
         }
+        
     }
 }
