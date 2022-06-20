9efcb67d433173112b45ba943231be9e6c5b3000
j15935720033 <99215381@ulive.pccu.edu.tw>
2022-06-20 08:45:13 +0800

    update

--- unity_20220606/Assets/Script/APIstatic.cs
+++ unity_20220606/Assets/Script/APIstatic.cs
@@ -37,17 +37,17 @@ namespace chia
             //取得應用程式的平台
             string application = Application.platform.ToString();
             //取得2D物理睡眠臨界值設定為 10
-            Physics2D.timeToSleep=10;
+            Physics2D.timeToSleep = 10;
             //時間大小設定為 0.5(慢動作)
             Time.fixedDeltaTime = 0.5f;
             //對 9.999 去小數點 (不限制去除方式)
-            float f01=Mathf.Floor(9.999f);
+            float f01 = Mathf.Floor(9.999f);
             //取到小數第2位 
             double f02 = System.Math.Round(9.255, 2);
             double f03 = System.Math.Round(9.245, 2);
 
 
-            print("所有攝影機數量"+ AllCameraNum);
+            print("所有攝影機數量" + AllCameraNum);
             print("應用程式的平台" + application);
             print("取得2D物理睡眠臨界值" + Physics2D.timeToSleep);
             print("對 9.999 去小數點:" + f01);
@@ -57,18 +57,20 @@ namespace chia
         }
         private void Update()
         {
+            #region
             //取得是否輸入任意鍵
-            //bool getAnyKey =Input.anyKey;
             if (Input.anyKey)
             {
                 print("A key or mouse click has been detected");
             }
             //取得遊戲經過時間
-            float GameTime=Time.time;
+            float GameTime = Time.time;
             //print("<Color=yellow >遊戲時間:</Color >"+ GameTime);
-           
-           
+            #endregion
+
+
+
         }
-        
+
     }
 }
